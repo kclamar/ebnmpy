a17620299bb4312cf7092ed4c8fba41511a73559
kclamar <kclamar@connect.ust.hk>
2021-12-04 05:18:33 +0800

    add ebnm_fns

--- ebnmpy/opt_control_defaults.py
+++ ebnmpy/opt_control_defaults.py
@@ -1,2 +1,14 @@
+def nlm_control_defaults():
+    return dict(ndigit=8, stepmax=5, check_analyticals=False)
+
+
 def lbfgsb_control_defaults():
+    return dict()
+
+
+def trust_control_defaults():
+    return dict(rinit=2, rmax=5)
+
+
+def optimize_control_defaults():
     return dict()
