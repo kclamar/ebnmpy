bc821afe9d45ff728058568848f3cdbb67f2394a
kclamar <kclamar@connect.ust.hk>
2021-12-04 20:48:31 +0800

    add point normal tests

--- tests/test_normal.py
+++ tests/test_normal.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pytest import approx
+from utils import expect_equal, expect_false, expect_identical
 
 from ebnmpy.ebnm import ebnm
 from ebnmpy.ebnm_fns import ebnm_normal
@@ -14,25 +14,6 @@ x = rnorm(n, 0, 10 + s)
 true_mean = 0
 true_sd = 10
 true_g = dict(pi=1, mean=true_mean, sd=true_sd)
-
-
-def expect_identical(a, b):
-    if isinstance(a, dict) and isinstance(b, dict):
-        for key in set(a).union(b):
-            expect_identical(a[key], b[key])
-    else:
-        assert np.all(a == b)
-
-
-def expect_equal(current, target, tolerance=None):
-    if isinstance(current, dict) and isinstance(target, dict):
-        return expect_equal(current.values(), target.values(), tolerance)
-
-    assert current == approx(target, rel=tolerance, abs=tolerance)
-
-
-def expect_false(value):
-    assert not value
 
 
 def test_basic_functionality():
